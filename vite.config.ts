e2b019ae83ae76e026c819607e0dd9857b161855
Aylin <vahabovaylin@gmail.com>
2025-10-17 10:50:09 +1100

    Create FIT3179-style Vega-Lite choropleth visualization

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/threatened-aus-map/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -20,4 +20,8 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    port: 8080,
+    open: true
+  }
 })
